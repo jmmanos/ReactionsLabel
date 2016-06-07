182a569dcb546fc91c96c1f91421b466379ee8ca
jmmanos <john.m.manos@gmail.com>
2016-06-07 16:57:36 -0700

    Initial commit and testing GitHub integration.

--- ReactionsLabel/ViewController.swift
+++ ReactionsLabel/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		print("Testing initial commit.")
 	}
 
 	override func didReceiveMemoryWarning() {
